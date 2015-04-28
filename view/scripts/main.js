fabce33661b9b7072d7bcd2da148f86a5739750f
Fengda HUANG <h@phodal.com>
2015-04-28 15:11:45 +0800

    Add map view

--- view/scripts/main.js
+++ view/scripts/main.js
@@ -1,1 +1,42 @@
-console.log('\'Allo \'Allo!');
+var exampleNS = {};
+
+exampleNS.getRendererFromQueryString = function() {
+	var obj = {}, queryString = location.search.slice(1),
+		re = /([^&=]+)=([^&]*)/g, m;
+
+	while (m = re.exec(queryString)) {
+		obj[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+	}
+	if ('renderers' in obj) {
+		return obj['renderers'].split(',');
+	} else if ('renderer' in obj) {
+		return [obj['renderer']];
+	} else {
+		return undefined;
+	}
+};
+
+var view = new ol.View({
+	center: [12119653.781323666,4054689.6824535457],
+	zoom: 4
+});
+
+var controls = ol.control.defaults({rotate: false});
+var interactions = ol.interaction.defaults({altShiftDragRotate:false, pinchRotate:false});
+
+var map = new ol.Map({
+	controls: controls,
+	interactions: interactions,
+	layers: [
+		new ol.layer.Tile({
+			source: new ol.source.BingMaps({
+				key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
+				culture: 'zh-CN',
+				imagerySet: 'Road'
+			})
+		})
+	],
+	renderer: exampleNS.getRendererFromQueryString(),
+	target: 'map',
+	view: view
+});
