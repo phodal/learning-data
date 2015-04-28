29138c3c065d8a9e6881eec378c05d8d711b80db
Fengda HUANG <h@phodal.com>
2015-04-28 09:29:25 +0800

    add geo function

--- nginx/jython-load.pig
+++ nginx/jython-load.pig
@@ -18,6 +18,6 @@ LOGS_BASE = FOREACH RAW_LOGS GENERATE
         useragent: chararray
     );
 
-A = FOREACH LOGS_BASE GENERATE ToDate(timestamp, 'dd/MMM/yyyy:HH:mm:ss Z') as date, ip, utils.query(url) as url,(int)status,(int)bytes,referrer,useragent;
+A = FOREACH LOGS_BASE GENERATE ToDate(timestamp, 'dd/MMM/yyyy:HH:mm:ss Z') as date, utils.location(ip) as ip, utils.query(url) as url,(int)status,(int)bytes,referrer,useragent;
 
 STORE A INTO 'log';
--- nginx/utils.py
+++ nginx/utils.py
@@ -1,9 +1,23 @@
-import urllib2
- 
+#!/usr/bin/python
+
+import sys
+sys.path.append('/Users/fdhuang/py27/lib/python2.7/site-packages/')
+import pygeoip
+
+
 @outputSchema('everything:chararray')
 def query(url):
     try:
-        print url
         return url
+    except:
+        pass
+
+@outputSchema('location:chararray')
+def location(ip):
+    try:
+        gi = pygeoip.GeoIP("data/GeoLiteCity.dat")
+        location = gi.region_by_addr(ip)
+        print location
+        return ip
     except:
         pass
