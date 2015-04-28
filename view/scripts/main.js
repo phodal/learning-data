7b9f7050d75cd7e4d3f2c804ad4114841bd3dc4a
Fengda HUANG <h@phodal.com>
2015-04-28 21:06:17 +0800

    set background color

--- view/scripts/main.js
+++ view/scripts/main.js
@@ -137,7 +137,7 @@ client.search(query).then(function(results){
 		radius = Math.min(width, height) / 2;
 
 	var color = d3.scale.ordinal()
-		.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+		.range(["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#4caf50", "#8bc34a", "#cddc39", "#3498db", "#2980b9", "#34495e", "#2c3e50", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#e74c3c", "#c0392b", "#f44336", "#e67e22", "#d35400", "#f39c12", "#ff9800", "#ff5722", "#ffc107", "#f1c40f", "#ffeb3b", "#9b59b6", "#8e44ad", "#9c27b0", "#673ab7", "#e91e63", "#3f51b5", "#795548", "#9e9e9e", "#607d8b", "#7f8c8d", "#95a5a6", "#bdc3c7", "#ecf0f1", "#efefef"]);
 
 	var arc = d3.svg.arc()
 		.outerRadius(radius - 10)
