0d5fc71c7af24b92cbe56f00705e134fdee7688a
Fengda HUANG <h@phodal.com>
2015-04-28 21:01:16 +0800

    Add d3.js for show citys

--- view/scripts/main.js
+++ view/scripts/main.js
@@ -59,7 +59,7 @@ var query = {
 			2: {
 				terms: {
 					field: "city",
-					size: 100,
+					size: 10,
 					order: {
 						_count: "desc"
 					}
@@ -131,4 +131,46 @@ client.search(query).then(function(results){
 			$(element).popover('destroy');
 		}
 	});
+
+	var width = 300,
+		height = 300,
+		radius = Math.min(width, height) / 2;
+
+	var color = d3.scale.ordinal()
+		.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+	var arc = d3.svg.arc()
+		.outerRadius(radius - 10)
+		.innerRadius(0);
+
+	var pie = d3.layout.pie()
+		.sort(null)
+		.value(function(d) { return d.doc_count; });
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	var citys = results.aggregations[2].buckets;
+	citys.forEach(function(city) {
+		city.doc_count = +city.doc_count;
+	});
+
+	var g = svg.selectAll(".arc")
+		.data(pie(citys))
+		.enter().append("g")
+		.attr("class", "arc");
+
+	g.append("path")
+		.attr("d", arc)
+		.style("fill", function(city) { return color(city.data.key); });
+
+	g.append("text")
+		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("dy", ".35em")
+		.style("text-anchor", "middle")
+		.text(function(d) { return d.data.key; });
+
 });
